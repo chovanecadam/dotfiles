e09d5ec4e2af3a066d77d5859ea228d835a43aaf
Adam Chovanec <adamachovanec@gmail.com>
2020-11-06 15:10:06 +0100

    .

---
+++ setup.sh
@@ -0,0 +1,33 @@
+# hardlinking dotfiles
+
+ln -f ~/.gitconfig     ~/dotfiles/.gitconfig
+ln -f ~/.zshrc         ~/dotfiles/.zshrc
+ln -f ~/.screenrc      ~/dotfiles/.screenrc
+ln -f ~/.tmux.conf     ~/dotfiles/.tmux.conf
+
+if [[ ! -d "~/.config/nvim/colors" ]]
+then
+    mkdir -p ~/.config/nvim/colors
+fi
+
+ln -f ~/.config/nvim/init.vim   ~/dotfiles/init.vim
+ln -f ~/.config/nvim/colors/myown.vim     ~/dotfiles/myown.vim
+
+# zsh setup
+
+if [[ ! "dpkg -s zsh | head -n 2 | grep installed" ]]
+then
+    sudo apt install zsh
+fi
+
+if [[ ! -d "~/.zsh/" ]]
+then 
+    mkdir ~/.zsh
+fi
+
+ln -f ~/zsh/zshsyntax.conf          ~/dotfiles/zshsyntax.conf
+ln -f ~/zsh/zshalias.conf           ~/dotfiles/zshalias.conf
+ln -f ~/zsh/watson.zsh-completion   ~/dotfiles/watson.zsh-completion
+
+chsh -s /bin/zsh
+
